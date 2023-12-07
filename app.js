de90ad92d1944df397698f7e6d3a36c58c8d3634
ShivanG <shivangramola007@gmail.com>
2023-12-07 17:51:28 +0530

    new

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const password = "1234567899";
+const ask = prompt("Enter password");
+
+if (ask === null || ask !== password) {
+  alert("Incorrect password. The page will be hidden.");
+  document.body.style.display = "none";
+} else {
+  document.body.style.display = "block";
+}
+
 const form = document.querySelector("form");
 const allMess = document.querySelector(".all-data");
 const result = document.querySelector(".result");
